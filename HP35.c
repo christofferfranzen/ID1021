9ed38928a2ac1f307226ef2f602a73dc68000106
Christoffer Franzén <cfranze@kth.se>
2023-08-29 15:20:07 +0200

    Header and base code

--- HP35.c
+++ HP35.c
@@ -1,6 +1,37 @@
-#include <stdio.h>
+//---------------------------------------------
+// Filnamn: HPC45.c
+// Syfte:
+//
+// Indata: En sträng med Reverse Polish notation
+// Utdata: Uträkningen
+//
+// Programmerare: Christoffer Franzén
+// Datum: 230829
+// Version: 1.0
+//---------------------------------------------
 
-int main() {
-  printf("Hello World!");
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10
+
+enum ItemType { ADD, SUB, MUL, DIV, VALUE };
+
+int ip = -1;
+void push();
+void pop(); 
+
+int main(int argc, char *argv[]) {
+
+
+  printf("%d", atoi(argv[1]));
   return 0;
 } 
+
+void push(){
+
+}
+
+void pop(){
+
+}
